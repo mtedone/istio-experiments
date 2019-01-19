de3856a10eb4b6f378c3a232d330a724851f0c39
Marco Tedone <marco.tedone@gmail.com>
2019-01-19 21:05:08 +0000

    First commit

---
+++ src/main/java/com/devopsfolks/istio/demo/istiodemo/IstioDemoApplication.java
@@ -0,0 +1,14 @@
+package com.devopsfolks.istio.demo.istiodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstioDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IstioDemoApplication.class, args);
+    }
+
+}
+
