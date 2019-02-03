292ea0d1f442dc87a8fb99cc78fc87852d803c4f
Marco Tedone <marco.tedone@gmail.com>
2019-02-03 18:03:17 +0000

    Completed first working example

---
+++ Docker/startup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export FILEBEAT_HOME=/filebeat-6.6.0-linux-x86_64
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.6.0-linux-x86_64.tar.gz
+
+tar xzvf filebeat-6.6.0-linux-x86_64.tar.gz
+
+rm -f filebeat-6.6.0-linux-x86_64.tar.gz
+
+cp /etc/filebeat/filebeat.yml $FILEBEAT_HOME/filebeat.yml
+
+chown root $FILEBEAT_HOME/filebeat.yml
+
+mkdir -p /var/log/filebeat
+
+$FILEBEAT_HOME/filebeat -e 2>/var/log/filebeat/filebeat.log &
+
+java -jar /opt/app/japp.jar
--- build-and-deploy-to-k8s.sh
+++ build-and-deploy-to-k8s.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+kubectl delete -f k8s/istio-experiments-elk.yml
+
 mvn clean package && docker build . -f Docker/Dockerfile -t alzamabar/istio-experiments-elk:latest
 
 kubectl apply -f k8s/istio-experiments-elk.yml
